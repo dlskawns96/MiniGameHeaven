2f9fa54a070e07a31def3ae7945f0ba313713be1
dlskawns96@gmail.com <dlskawns96@gmail.com>
2016-11-18 18:03:19 +0900

    Login / Sign Up window UI frame

---
+++ MiniGameHeaven/src/LoginWindow.java
@@ -0,0 +1,99 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+
+public class LoginWindow extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField IDField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginWindow frame = new LoginWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LoginWindow() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		IDField = new JTextField();
+		IDField.setBounds(131, 76, 129, 24);
+		contentPane.add(IDField);
+		IDField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(131, 112, 129, 24);
+		contentPane.add(passwordField);
+		
+		
+		
+		JLabel lblId = new JLabel("ID");
+		lblId.setBounds(53, 79, 62, 18);
+		contentPane.add(lblId);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(53, 115, 77, 18);
+		contentPane.add(lblPassword);
+		
+		JButton signInBtn = new JButton("Sign in");
+		signInBtn.setFont(new Font("Gulim", Font.PLAIN, 15));
+		signInBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Sign In Button Clicked");
+			}
+		});
+		signInBtn.setBounds(274, 75, 105, 61);
+		contentPane.add(signInBtn);
+		
+		JButton FindBtn = new JButton("Forgot ID/PW");
+		FindBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Find Button Clicked");
+			}
+		});
+
+		FindBtn.setFont(new Font("±¼¸²", Font.PLAIN, 15));
+		FindBtn.setBounds(131, 148, 129, 27);
+		contentPane.add(FindBtn);
+		
+		JButton signUpBtn = new JButton("Sign Up");
+		signUpBtn.setFont(new Font("Gulim", Font.PLAIN, 15));
+		signUpBtn.setBounds(274, 148, 105, 27);
+		contentPane.add(signUpBtn);
+		signUpBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Sign Up Button Clicked");
+			}
+		});
+	}
+}
---
+++ MiniGameHeaven/src/SignUpWindow.java
@@ -0,0 +1,96 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class SignUpWindow extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField IDField;
+	private JTextField PWField;
+	private JTextField PWAgainField;
+	private JTextField emailField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SignUpWindow frame = new SignUpWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public SignUpWindow() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 403);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblId = new JLabel("ID (Will be used as your nickname)");
+		lblId.setFont(new Font("±¼¸²", Font.BOLD, 15));
+		lblId.setBounds(72, 46, 310, 18);
+		contentPane.add(lblId);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setFont(new Font("±¼¸²", Font.BOLD, 15));
+		lblPassword.setBounds(72, 103, 310, 18);
+		contentPane.add(lblPassword);
+		
+		JLabel lblRep = new JLabel("Password Again");
+		lblRep.setFont(new Font("±¼¸²", Font.BOLD, 15));
+		lblRep.setBounds(72, 159, 310, 18);
+		contentPane.add(lblRep);
+		
+		JLabel lblNewLabel = new JLabel("Email Adress");
+		lblNewLabel.setFont(new Font("±¼¸²", Font.BOLD, 15));
+		lblNewLabel.setBounds(72, 219, 310, 18);
+		contentPane.add(lblNewLabel);
+		
+		IDField = new JTextField();
+		IDField.setBounds(72, 67, 209, 24);
+		contentPane.add(IDField);
+		IDField.setColumns(10);
+		
+		PWField = new JTextField();
+		PWField.setBounds(72, 123, 209, 24);
+		contentPane.add(PWField);
+		PWField.setColumns(10);
+		
+		PWAgainField = new JTextField();
+		PWAgainField.setBounds(72, 189, 209, 24);
+		contentPane.add(PWAgainField);
+		PWAgainField.setColumns(10);
+		
+		emailField = new JTextField();
+		emailField.setBounds(72, 249, 209, 24);
+		contentPane.add(emailField);
+		emailField.setColumns(10);
+		
+		JButton IDCheckBtn = new JButton("Check");
+		IDCheckBtn.setBounds(293, 66, 73, 27);
+		contentPane.add(IDCheckBtn);
+		
+		JButton signUpBtn = new JButton("Sign Up");
+		signUpBtn.setBounds(164, 304, 105, 27);
+		contentPane.add(signUpBtn);
+	}
+
+}
