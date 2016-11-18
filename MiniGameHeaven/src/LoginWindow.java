45c08193a1da45c176bf7ab06a57795d94dd9748
dlskawns96 <dlskawns96@gmail.com>
2016-11-19 02:11:23 +0900

    //DISPOSE_ON_CLOSE : 창 한번에 안닫히게

--- MiniGameHeaven/src/LoginWindow.java
+++ MiniGameHeaven/src/LoginWindow.java
@@ -39,7 +39,7 @@ public class LoginWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public LoginWindow() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
--- MiniGameHeaven/src/SignUpWindow.java
+++ MiniGameHeaven/src/SignUpWindow.java
@@ -34,7 +34,7 @@ public class SignUpWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public SignUpWindow() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 403);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
