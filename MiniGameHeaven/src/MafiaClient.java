96840cdc3f2a0f7d0f2835c6dcf0a04444bcac6c
dlskawns96 <dlskawns96@gmail.com>
2016-12-06 01:04:15 +0900

    Mafia 75%

--- MiniGameHeaven/src/MafiaClient.java
+++ MiniGameHeaven/src/MafiaClient.java
@@ -8,177 +8,187 @@ import javax.swing.border.EmptyBorder;
 import java.awt.event.*;
 import java.awt.Color;
 
+public class MafiaClient extends JFrame implements ActionListener {
 
-public class MafiaClient extends JFrame implements ActionListener{
+	private JPanel contentPane;
+	BufferedReader in;
+	PrintWriter out;
+	private JTextField textField;
+	private JTextArea messageArea;
+	private String ID;
+	private String IP = "";
+	private String role;
+	private int roomNum;
+	private static ArrayList<MafiaPlayer> players = new ArrayList<MafiaPlayer>();
+	private static int r, g, b;
+	private Timer timer = new Timer(10, this);
+	private JLabel dayOrnight;
+	private boolean isMaster = false;
+	private int people = 0;
+	private JLabel peopleInRoom;
+	private JButton gameStartBtn;
+	private JLabel roleLabel;
+	private JButton voteBtn;
+	/**
+	 * Launch the application.
+	 * 
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
 
-   private JPanel contentPane;
-   BufferedReader in;
-   PrintWriter out;
-   private JTextField textField;
-   private JTextArea messageArea;
-   private String ID;
-   private String IP = "";
-   private String role;
-   private int roomNum;
-   private static ArrayList<MafiaPlayer> players = new ArrayList<MafiaPlayer>();
-   private static int r,g,b;
-   private  Timer timer = new Timer(10, this);
-   private JLabel dayOrnight;
-   private boolean isMaster = false;
-   private int people = 0;
-   private JLabel peopleInRoom;
-   
-   /**
-    * Launch the application.
-    * @throws IOException 
-    */
-   public static void main(String[] args) throws IOException {
-      
-	   //for test
-      MafiaClient a = new MafiaClient("11","127.0.0.1",3, true);
-      a.run();
-   }
+		// for test
+		MafiaClient a = new MafiaClient("17", "127.0.0.1", 3, false);
+		a.run();
+	}
 
-   /**
-    * Create the frame.
-    */
-   public MafiaClient(String id, String ip, int room, boolean isMaster) {
-      
-	   
-	   this.isMaster = isMaster;
-	   this.ID = id;
-	   this.IP = ip;
-	   this.roomNum = room;
+	/**
+	 * Create the frame.
+	 */
+	public MafiaClient(String id, String ip, int room, boolean isMaster) {
 
-      setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-      setBounds(100, 100, 760, 528);
-      contentPane = new JPanel();
-      contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-      setContentPane(contentPane);
-      contentPane.setLayout(null);
-      
-      textField = new JTextField();
-      textField.setBounds(14, 435, 504, 24);
-      contentPane.add(textField);
-      textField.setColumns(10);
-      
-      
-      messageArea = new JTextArea();
-      messageArea.setEditable(false);
-      
-      
-      JScrollPane scrollBar = new JScrollPane(messageArea);
-      scrollBar.setBackground(Color.WHITE);
-      scrollBar.setBounds(14, 50, 504, 373);
-      scrollBar.setOpaque(false);
-      scrollBar.setViewportView(messageArea);
-      contentPane.add(scrollBar);
-      
-      
-      JButton chatBtn = new JButton("Enter");
-      chatBtn.setBounds(525, 434, 105, 27);
-      contentPane.add(chatBtn);
-      
-      JScrollPane userScroll = new JScrollPane();
-      userScroll.setBounds(532, 84, 196, 136);
-      contentPane.add(userScroll);
-      
-      JTextArea playerList = new JTextArea();
-      playerList.setEditable(false);
-      userScroll.setViewportView(playerList);
-      
-      JButton gameStartBtn = new JButton("Game Start");
-      gameStartBtn.setEnabled(isMaster);
-      gameStartBtn.setBounds(525, 372, 105, 50);
-      contentPane.add(gameStartBtn);
-      
-      dayOrnight = new JLabel();
-      dayOrnight.setBounds(14, 10, 105, 30);
-      dayOrnight.setBackground(Color.white);
-      contentPane.add(dayOrnight);
-      
-      peopleInRoom = new JLabel("");
-      peopleInRoom.setBounds(532, 55, 196, 15);
-      contentPane.add(peopleInRoom);
-      
-      //엔터 눌렀을때 액션
-       textField.addActionListener(new ActionListener() {
-               public void actionPerformed(ActionEvent e) {
-                   out.println(roomNum+" "+textField.getText());
-                   textField.setText("");
-               }
-           });
-       //채팅 버튼 눌렀을때 액션
-       chatBtn.addActionListener(new ActionListener() {
-     	  public void actionPerformed(ActionEvent e) {
-               out.println(roomNum+" "+textField.getText());
-               textField.setText("");
-           }
-       });
-       //게임 시작 버튼 액션
-       gameStartBtn.addActionListener(new ActionListener() {
-    	   public void actionPerformed(ActionEvent e) {
-    		   out.println(roomNum + " GameStart");
-    	   }
-       });
+		this.isMaster = isMaster;
+		this.ID = id;
+		this.IP = ip;
+		this.roomNum = room;
 
-        setVisible(true);
-        
-  
-   }
-   
-   private void run() throws IOException {
-	   
-	  
-	   r = 255;
-	   g = 255;
-	   b = 255;
-        // Make connection and initialize streams
-        Socket socket = new Socket(IP, 9997);
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        out = new PrintWriter(socket.getOutputStream(), true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 760, 528);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 
-        // Process all messages from server, according to the protocol.
-        while (true) {
-            String line = in.readLine();
-            if (line.startsWith("SUBMITNAME")) {
-                out.println(ID);
-            } else if(line.startsWith("SUBMITROOM")) {
-            	if(isMaster)
-            		out.println(roomNum + " MASTER");
-            	else
-            		out.println(roomNum+"");
-            } else if (line.startsWith("NAMEACCEPTED")) {
-                textField.setEditable(true);
-            } else if (line.startsWith("MESSAGE")) {
-                messageArea.append(line.substring(8) + "\n");
-            } else if(line.startsWith("<SYSTEM>")) { //handle system messages that notices entering and leaving
-            	peopleInRoom.setText("People In Room : " + line.split(" ")[line.split(" ").length - 1]);
-               messageArea.append(line + "\n");
-            } else if(line.startsWith("From") || line.startsWith("To")) { //handle whispering messages
-               messageArea.append(line +"\n");               
-            } else if(line.startsWith("GAMESTART")) {
-            	
-            	mafiaStart();    	
-            }
-            messageArea.setCaretPosition(messageArea.getDocument().getLength());
-        }
-    }
-   
-   
-   private void mafiaStart()
-   {
-	   timer.start();
-   }
-   
-   
-   @Override
-   public void actionPerformed(ActionEvent e) {
-       System.out.println(r);
-       contentPane.setBackground(new Color(r--,g--,b--));
-       dayOrnight.setText("Day left : "+(r-9));
-       dayOrnight.setForeground(new Color(255-r,255-g,255-b));
-       if(r < 10)
-   		  timer.stop();
-   }
+		textField = new JTextField();
+		textField.setBounds(14, 435, 504, 24);
+		contentPane.add(textField);
+		textField.setColumns(10);
+
+		messageArea = new JTextArea();
+		messageArea.setEditable(false);
+
+		JScrollPane scrollBar = new JScrollPane(messageArea);
+		scrollBar.setBackground(Color.WHITE);
+		scrollBar.setBounds(14, 50, 504, 373);
+		scrollBar.setOpaque(false);
+		scrollBar.setViewportView(messageArea);
+		contentPane.add(scrollBar);
+
+		JButton chatBtn = new JButton("Enter");
+		chatBtn.setBounds(525, 433, 105, 27);
+		contentPane.add(chatBtn);
+
+		JScrollPane userScroll = new JScrollPane();
+		userScroll.setBounds(532, 50, 196, 136);
+		contentPane.add(userScroll);
+
+		JTextArea playerList = new JTextArea();
+		playerList.setEditable(false);
+		userScroll.setViewportView(playerList);
+
+		gameStartBtn = new JButton("Game Start");
+		gameStartBtn.setBounds(525, 372, 105, 50);
+		gameStartBtn.setEnabled(isMaster);
+		contentPane.add(gameStartBtn);
+
+		dayOrnight = new JLabel();
+		dayOrnight.setBounds(14, 10, 105, 30);
+		dayOrnight.setBackground(Color.white);
+		contentPane.add(dayOrnight);
+
+		peopleInRoom = new JLabel("");
+		peopleInRoom.setBounds(532, 55, 196, 15);
+		contentPane.add(peopleInRoom);
+		
+		roleLabel = new JLabel();
+		roleLabel.setBounds(530, 349, 100, 15);
+		contentPane.add(roleLabel);
+		
+		voteBtn = new JButton("Vote for Kill");
+		voteBtn.setBounds(579, 222, 97, 61);
+		
+
+		// 엔터 눌렀을때 액션
+		textField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				out.println(roomNum + " " + textField.getText());
+				textField.setText("");
+			}
+		});
+		// 채팅 버튼 눌렀을때 액션
+		chatBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				out.println(roomNum + " " + textField.getText());
+				textField.setText("");
+			}
+		});
+		// 게임 시작 버튼 액션
+		gameStartBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				out.println(roomNum + " GameStart");
+			}
+		});
+
+		setVisible(true);
+
+	}
+
+	private void run() throws IOException {
+
+		r = 255;
+		g = 255;
+		b = 255;
+		// Make connection and initialize streams
+		Socket socket = new Socket(IP, 9997);
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		out = new PrintWriter(socket.getOutputStream(), true);
+
+		// Process all messages from server, according to the protocol.
+		while (true) {
+			String line = in.readLine();
+			if (line.startsWith("SUBMITNAME")) {
+				out.println(ID);
+			} else if (line.startsWith("SUBMITROOM")) {
+				if (isMaster)
+					out.println(roomNum + " MASTER");
+				else
+					out.println(roomNum + "");
+			} else if (line.startsWith("NAMEACCEPTED")) {
+				textField.setEditable(true);
+			} else if (line.startsWith("MESSAGE")) {
+				messageArea.append(line.substring(8) + "\n");
+			} else if (line.startsWith("<SYSTEM>")) { 
+				peopleInRoom.setText("People In Room : " + line.split(" ")[line.split(" ").length - 1]);
+				messageArea.append(line + "\n");
+			} else if (line.startsWith("From") || line.startsWith("To")) { 
+				messageArea.append(line + "\n");
+			} else if (line.startsWith("GAMESTART")) {
+				gameStartBtn.setEnabled(false);
+				mafiaStart();
+			}
+			messageArea.setCaretPosition(messageArea.getDocument().getLength());
+		} 
+	}
+
+	private void mafiaStart() throws IOException {
+		out.println("ROLE");
+		role = in.readLine();
+		roleLabel.setText("You are " + role.toUpperCase());
+		timer.setDelay(1000);
+		timer.start();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println(r);
+		contentPane.setBackground(new Color(r--, g--, b--));
+		dayOrnight.setText("Day left : " + (r - 9));
+		dayOrnight.setForeground(new Color(255 - r, 255 - g, 255 - b));
+		peopleInRoom.setForeground(new Color(255 - r, 255 - g, 255 - b));
+		if (r < 10)
+		{				
+			timer.stop();
+			contentPane.add(voteBtn);
+			contentPane.remove(voteBtn);
+		}
+	}
 }
--- MiniGameHeaven/src/MafiaServer.java
+++ MiniGameHeaven/src/MafiaServer.java
@@ -22,180 +22,238 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 
-
 public class MafiaServer extends JFrame {
 
-    private static final int PORT = 9997;
+	private static final int PORT = 9997;
 
-    private static HashSet<String> names = new HashSet<String>();
+	private static HashSet<String> names = new HashSet<String>();
 
-    private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
+	private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
 
-    private static List<MafiaPlayer> players = new ArrayList<MafiaPlayer>();
-    
-    private static List<MafiaRoom> rooms = new ArrayList<MafiaRoom>();
-    
-    public static void main(String[] args) throws Exception {
-        System.out.println("The Mafia server is running.");
-        
-        ServerSocket listener = new ServerSocket(PORT);
-        try {
-            while (true) {
-                new Handler(listener.accept()).start();
-            }
-        } finally {
-            listener.close();
-        }
-    }
+	private static List<MafiaPlayer> players = new ArrayList<MafiaPlayer>();
 
-    private static class Handler extends Thread {
-        private String name;
-        private Socket socket;
-        private BufferedReader in;
-        private PrintWriter out;
-        private int roomNum;
-        private int people;
-        private String input;
-        MafiaPlayer tmp = new MafiaPlayer();
-        MafiaRoom tmpRoom = new MafiaRoom();
-        public Handler(Socket socket) {
-            this.socket = socket;
-        }
+	private static List<MafiaRoom> rooms = new ArrayList<MafiaRoom>();
+	
+	
+	public static void main(String[] args) throws Exception {
+		System.out.println("The Mafia server is running.");
 
-        public void run() {
-            try {
+		ServerSocket listener = new ServerSocket(PORT);
+		try {
+			while (true) {
+				new Handler(listener.accept()).start();
+			}
+		} finally {
+			listener.close();
+		}
+	}
 
-                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                out = new PrintWriter(socket.getOutputStream(), true);
-                
-                while (true) {
-                    out.println("SUBMITNAME");
-                    name = in.readLine();
-                    if (name == null) {
-                        return;
-                    }
-                    tmp.name = name;
-                    synchronized (names) {
-                        if (!names.contains(name)) {                        
-                            names.add(name);
-                            break;
-                        }
-                    }
-                }
-                
-                out.println("SUBMITROOM");
-                input = in.readLine();
-                if(input.endsWith("MASTER")) //방장 등장
-                {
-                    roomNum = Integer.parseInt(input.split(" ")[0]);
-                	tmpRoom.roomNumber = Integer.parseInt(input.split(" ")[0]);
-                	tmpRoom.peopleNum++;
-                	people = 1;
-                	rooms.add(tmpRoom);
-                }
-                else
-                {
-                	roomNum = Integer.parseInt(input);
-                	for(int i = 0; i < rooms.size(); i++)
-                	{
-                		if(rooms.get(i).roomNumber == Integer.parseInt(input))
-                		{
-                			tmpRoom.roomNumber = rooms.get(i).roomNumber;
-                			tmpRoom.peopleNum = rooms.get(i).peopleNum + 1;
-                			rooms.set(i, tmpRoom);
-                			people = rooms.get(i).peopleNum;
-                			break;
-                		}
-                			
-                	}
-                }
-                System.out.println(people);
-                tmp.roomNumber = roomNum;
-                
-                out.println("NAMEACCEPTED");
-                writers.add(out);
-                
-                tmp.writer = out;
-                players.add(tmp);
-            
-                for(int i = 0; i < players.size(); i++) {
-                    tmp = players.get(i);
-                    if(roomNum == tmp.roomNumber) 
-                    	tmp.writer.println("<SYSTEM> " + name + " Is entered. " + Integer.toString(people));
-                 }
-                
-                // Accept messages from this client and broadcast them.
-                // Ignore other clients that cannot be broadcasted to.
-                while (true) {
-                    input = in.readLine();
-                    System.out.println(input);
-                    if (input == null) {
-                        return;
-                    }
-                    //whispering
-                    else if(input.startsWith("/w")) {
-                       if(input.split(" ").length < 2)
-                          continue;
-                       
-                       int i;
-                       //find index of reciever's name in clients List
-                       for(i = 0; i < players.size(); i++) {
-                          tmp = players.get(i);
-                          if(input.split(" ")[1].equals(tmp.name)) 
-                             break;
-                       }
+	private static class Handler extends Thread {
+		private String name;
+		private Socket socket;
+		private BufferedReader in;
+		private PrintWriter out;
+		private int roomNum;
+		private int people;
+		private String input;
+		MafiaPlayer tmp = new MafiaPlayer();
+		MafiaRoom tmpRoom = new MafiaRoom();
 
-                       String whisper = "From <" + name + "> " + input.split(" ", 3)[2];
-                       tmp.writer.println(whisper);
-                       
-                       //find index of sender's name in clients List
-                       for(i = 0; i < players.size(); i++) {
-                          tmp = players.get(i);
-                          if(tmp.name.equals(name)) 
-                             break;
-                       }
+		public Handler(Socket socket) {
+			this.socket = socket;
+		}
 
-                       whisper = "To <" + input.split(" ")[1] + "> " + input.split(" ", 3)[2];
-                       tmp.writer.println(whisper);
-                          
-                       continue;
-                    }
-                    
-                    //일반 메시지 처리
-                    for(int i = 0; i < players.size(); i++) {
-                        tmp = players.get(i);
-                        if(input.split(" ")[0].equals(Integer.toString(tmp.roomNumber))) 
-                           tmp.writer.println("MESSAGE " + name + ": " + input.substring(2));
-                     }
-                    //게임 스타트 처리
-                    if(input.endsWith("GameStart"))
-                    {
-                    	  for(int i = 0; i < players.size(); i++) {
-                              tmp = players.get(i);
-                              if(input.split(" ")[0].equals(Integer.toString(tmp.roomNumber))) 
-                                 tmp.writer.println("GAMESTART");
-                           }
-                    }
-                    
-                }
-            } catch (IOException e) {
-                System.out.println(e);
-            } finally {
-                
-               for (PrintWriter writer : writers) {
-                  writer.println("<SYSTEM> " + name + " Is left. ");
-               }
-                if (name != null) {
-                    names.remove(name);
-                }
-                if (out != null) {
-                    writers.remove(out);
-                }
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                }
-            }
-        }
-    }
+		public void run() {
+			try {
+
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				out = new PrintWriter(socket.getOutputStream(), true);
+
+				while (true) {
+					out.println("SUBMITNAME");
+					name = in.readLine();
+					if (name == null) {
+						return;
+					}
+					tmp.name = name;
+					synchronized (names) {
+						if (!names.contains(name)) {
+							names.add(name);
+							break;
+						}
+					}
+				}
+
+				out.println("SUBMITROOM");
+				input = in.readLine();
+				if (input.endsWith("MASTER")) // 방장 등장
+				{
+					roomNum = Integer.parseInt(input.split(" ")[0]);
+					tmpRoom.roomNumber = Integer.parseInt(input.split(" ")[0]);
+					tmpRoom.peopleNum++;
+					people = 1;
+					rooms.add(tmpRoom);
+				} else {
+					roomNum = Integer.parseInt(input);
+					for (int i = 0; i < rooms.size(); i++) {
+						if (rooms.get(i).roomNumber == Integer.parseInt(input)) {
+							tmpRoom.roomNumber = rooms.get(i).roomNumber;
+							tmpRoom.peopleNum = rooms.get(i).peopleNum + 1;
+							rooms.set(i, tmpRoom);
+							people = rooms.get(i).peopleNum;
+							break;
+						}
+					}
+				}
+				System.out.println(people);
+				tmp.roomNumber = roomNum;
+
+				out.println("NAMEACCEPTED");
+				writers.add(out);
+
+				tmp.writer = out;
+				tmp.role = "null";
+				players.add(tmp);
+
+				for (int i = 0; i < players.size(); i++) {
+					tmp = players.get(i);
+					if (roomNum == tmp.roomNumber)
+						tmp.writer.println("<SYSTEM> " + name + " Is entered. " + Integer.toString(people));
+				}
+
+				// Accept messages from this client and broadcast them.
+				// Ignore other clients that cannot be broadcasted to.
+				while (true) {
+					input = in.readLine();
+					System.out.println(input);
+					if (input == null) {
+						return;
+					}
+					
+					// 일반 메시지 처리
+					for (int i = 0; i < players.size(); i++) {
+						tmp = players.get(i);
+						if (input.split(" ")[0].equals(Integer.toString(tmp.roomNumber)))
+							tmp.writer.println("MESSAGE " + name + ": " + input.substring(2));
+					}
+					// 게임 스타트 처리
+					if (input.endsWith("GameStart")) {
+						int k;
+						for (k = 0; k < rooms.size(); k++) {
+							if (rooms.get(k).roomNumber == Integer.parseInt(input.split(" ")[0])) {
+								if (rooms.get(k).peopleNum != 7)
+									break;
+							}
+						}
+						if(k != rooms.size())
+						{	
+							for (int i = 0; i < players.size(); i++) {
+								tmp = players.get(i);
+								if (input.split(" ")[0].equals(Integer.toString(tmp.roomNumber)))
+									tmp.writer.println("MESSAGE " + "You Need 7 people");
+							}
+						}
+						else
+						{
+							for (int i = 0; i < players.size(); i++) {
+								tmp = players.get(i);
+								if (input.split(" ")[0].equals(Integer.toString(tmp.roomNumber)))
+									tmp.writer.println("GAMESTART");
+							}
+						}
+					}
+					
+					//역할 분배
+					if(input.startsWith("ROLE"))
+					{
+						disRole(Integer.parseInt(input.split(" ")[0]));
+					}
+
+				}
+			} catch (IOException e) {
+				System.out.println(e);
+			} finally {
+				System.out.println("EXIT SOMEONE");
+
+				for (int i = 0; i < rooms.size(); i++) {
+					if (rooms.get(i).roomNumber == roomNum) {
+						tmpRoom.roomNumber = rooms.get(i).roomNumber;
+						tmpRoom.peopleNum = rooms.get(i).peopleNum - 1;
+						rooms.set(i, tmpRoom);
+						people = rooms.get(i).peopleNum;
+						break;
+					}
+				}
+
+				for (PrintWriter writer : writers) {
+					writer.println("<SYSTEM> " + name + " Is left. " + Integer.toString(people));
+				}
+				if (name != null) {
+					names.remove(name);
+				}
+				if (out != null) {
+					writers.remove(out);
+				}
+				try {
+					socket.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+	}
+	
+	private static void disRole(int rn)
+	{
+		List<MafiaPlayer> playersInRoom = new ArrayList<MafiaPlayer>();
+		MafiaPlayer tmp = new MafiaPlayer();
+		
+		Random rand = new Random();
+		int r;
+		int mafia = 2, civil = 3;
+		
+		for (int i = 0; i < players.size(); i++) {
+			tmp = players.get(i);
+			if (rn == tmp.roomNumber)
+				playersInRoom.add(tmp);
+		}
+		
+		for(;mafia > 0; mafia--)
+		{
+			r = rand.nextInt(playersInRoom.size());
+			while(playersInRoom.get(r).role != "null")
+				r = rand.nextInt(playersInRoom.size());
+			tmp = playersInRoom.get(r);
+			tmp.role = "mafia";
+			tmp.writer.println("mafia");
+			playersInRoom.set(r, tmp);
+		}		
+		
+		r = rand.nextInt(playersInRoom.size());
+		while(playersInRoom.get(r).role != "null")
+			r = rand.nextInt(playersInRoom.size());
+		tmp = playersInRoom.get(r);
+		tmp.role = "police";
+		tmp.writer.println("police");
+		playersInRoom.set(r, tmp);
+		
+		r = rand.nextInt(playersInRoom.size());
+		while(playersInRoom.get(r).role != "null")
+			r = rand.nextInt(playersInRoom.size());
+		tmp = playersInRoom.get(r);
+		tmp.role = "doctor";
+		tmp.writer.println("doctor");
+		playersInRoom.set(r, tmp);
+		
+		for(;civil > 0; civil--)
+		{
+			r = rand.nextInt(playersInRoom.size());
+			while(playersInRoom.get(r).role != "null")
+				r = rand.nextInt(playersInRoom.size());
+			tmp = playersInRoom.get(r);
+			tmp.role = "civil";
+			tmp.writer.println("civil");
+			playersInRoom.set(r, tmp);
+		}
+		
+	}
 }
